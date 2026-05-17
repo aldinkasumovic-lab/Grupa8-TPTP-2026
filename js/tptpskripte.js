30d9424225b5771987cf5c4ca8938d466931e2b9
alejnaselimovic <alejna.selimovic@fet.ba>
2026-05-17 22:37:56 +0200

    muskarci, zene.. tptpskripte.js

--- js/tptpskripte.js
+++ js/tptpskripte.js
@@ -40,4 +40,41 @@ if (localStorage.getItem('darkMode') === 'on') {
   document.body.classList.add('dark-mode');
   document.querySelector('.dark-toggle').textContent = '☀️ Light Mode';
 }
-//
+//muskarci zene djeca kupuj po igracu
+var tabDugmad = document.querySelectorAll('.tab-dugme');
+var kartice = document.querySelectorAll('.shop-kartica');
+var nemaRezultataPoruka = document.getElementById('nema-rezultata'); 
+
+for (var i = 0; i < tabDugmad.length; i++) {
+    tabDugmad[i].addEventListener('click', function() {
+        
+        
+        for (var j = 0; j < tabDugmad.length; j++) {
+            tabDugmad[j].classList.remove('aktivan');
+        }
+        
+        this.classList.add('aktivan');
+
+        var izabranaKategorija = this.getAttribute('data-tab');
+        var brojPronadjenih = 0; // Brojač koji prati ima li artikala u toj kategoriji
+
+        
+        for (var k = 0; k < kartice.length; k++) {
+            var kategorijaKartice = kartice[k].getAttribute('data-kategorija');
+
+            if (izabranaKategorija === kategorijaKartice) {
+                kartice[k].style.display = ''; 
+                brojPronadjenih++;
+            } else {
+                kartice[k].style.display = 'none'; 
+            }
+        }
+
+        //  Prikazivanje poruke za "Nema proizvoda"
+        if (brojPronadjenih === 0) {
+            nemaRezultataPoruka.style.display = 'block';
+        } else {
+            nemaRezultataPoruka.style.display = 'none';
+        }
+    });
+}
