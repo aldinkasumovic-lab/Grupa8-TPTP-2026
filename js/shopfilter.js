92e54126809fd7721dbe8ac493c878c87d0f40ef
alejnaselimovic <alejna.selimovic@fet.ba>
2026-05-17 21:50:55 +0200

    ispravak shopfilter.js

--- js/shopfilter.js
+++ js/shopfilter.js
@@ -1,27 +1,42 @@
 var tabDugmad = document.querySelectorAll('.tab-dugme');
 var kartice = document.querySelectorAll('.shop-kartica');
+var nemaRezultataPoruka = document.getElementById('nema-rezultata'); 
 
 for (var i = 0; i < tabDugmad.length; i++) {
     tabDugmad[i].addEventListener('click', function() {
         
+        
         for (var j = 0; j < tabDugmad.length; j++) {
             tabDugmad[j].classList.remove('aktivan');
         }
+        
         this.classList.add('aktivan');
 
         var izabranaKategorija = this.getAttribute('data-tab');
+        var brojPronadjenih = 0; // Brojač koji prati ima li artikala u toj kategoriji
 
+        
         for (var k = 0; k < kartice.length; k++) {
             var kategorijaKartice = kartice[k].getAttribute('data-kategorija');
 
             if (izabranaKategorija === kategorijaKartice) {
-                kartice[k].style.display = 'block';
+                kartice[k].style.display = ''; 
+                brojPronadjenih++;
             } else {
-                kartice[k].style.display = 'none';
+                kartice[k].style.display = 'none'; 
             }
+        }
+
+        //  Prikazivanje poruke za "Nema proizvoda"
+        if (brojPronadjenih === 0) {
+            nemaRezultataPoruka.style.display = 'block';
+        } else {
+            nemaRezultataPoruka.style.display = 'none';
         }
     });
 }
 
-// kliknuti  na prvo dugme (Muškarci) da se sakriju ostali dresovi
-tabDugmad[0].click();
+// Odmah kliknuti na prvi tab (Muškarci) pri učitavanju stranice
+if (tabDugmad.length > 0) {
+    tabDugmad[0].click();
+}
